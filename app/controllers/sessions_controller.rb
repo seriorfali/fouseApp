39e32c81e1f97ac07a194a64ec0995a88efaa361
Seri <seriorfali@gmail.com>
2015-11-01 01:19:54 -0700

    Made login form partial, defined sessions controller start_for_existing action.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,22 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
+  def start_for_existing
+    @user = User.find_by(username: params[:session][:username])
+    @developer = Developer.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:member_id] = @user.id.to_s
+    elsif @developer && @developer.authenticate(params[:session][:password])
+      session[:member_id] = @developer.id.to_s
+    elsif params[:session][:type] == "Users"
+      RedirectToAction("New", "Users")
+    elsif params[:session][:type] == "Developer"
+      RedirectToAction("New", "Developers")
+    else
+      redirect_to login_path
+  end
+
+  def start_for_new
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,9 @@ class UsersController < ApplicationController
   def show
   end
 
+  def new
+  end
+
   def edit
   end
 end
